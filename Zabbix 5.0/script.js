f6c513d11433b2f9e7fb687e1d5c9cb617ee6670
Ryan Cocks <ryan@ryancocks.net>
2024-07-04 20:43:06 +0100

    Fist code to try

--- Zabbix 5.0/script.js
+++ Zabbix 5.0/script.js
@@ -1,81 +1,6 @@
-// NB, you can use console.log, warn, error in Zabbix code. c.f. https://www.zabbix.com/documentation/current/en/manual/config/items/preprocessing/javascript/javascript_objects
-
-/****************************************************************************** */
-/* Debug for when running on local machine: */
-/****************************************************************************** */
-const dotenv = require('dotenv');
-dotenv.config({ path: './.env' });
-const value = JSON.stringify({
-  ONE_UPTIME_URL: process.env.ONE_UPTIME_URL,
-  ONE_UPTIME_API_KEY: process.env.ONE_UPTIME_API_KEY,
-  ONE_UPTIME_PROJECT_ID: process.env.ONE_UPTIME_PROJECT_ID,
-  event_is_problem: 1,
-  event_is_update: 0,
-  event_severity: 1,
-});
-const { execSync } = require('child_process');
-
-// Code from GPT, not reviewed
-// function CurlHttpRequest(url, options = {}) {
-//   // Helper function to build the curl command with options
-//   const buildCurlCommand = (opts) => {
-//     let cmd = `curl -s -X ${opts.method || 'GET'}`;
-//     if (opts.headers) {
-//       for (const [key, value] of Object.entries(opts.headers)) {
-//         cmd += ` -H "${key}: ${value}"`;
-//       }
-//     }
-//     if (opts.data) {
-//       cmd += ` -d '${JSON.stringify(opts.data)}'`;
-//     }
-//     if (opts.proxy) {
-//       cmd += ` --proxy ${opts.proxy}`;
-//     }
-//     if (opts.auth) {
-//       cmd += ` -u '${opts.auth.username}:${opts.auth.password}'`;
-//     }
-//     cmd += ` ${url}`;
-//     return cmd;
-//   }
-
-//   // Execute the curl command and return the response
-//   const response = execSync(buildCurlCommand(options)).toString();
-//   const statusCode = parseInt(execSync('curl -o /dev/null -w %{http_code} ' + buildCurlCommand(options)));
-
-//   // Return an object with similar structure to CurlHttpRequest
-//   return {
-//     status() {
-//       return statusCode;
-//     },
-//     responseText() {
-//       return response;
-//     },
-//   };
-// }
-
-// Example usage
-const httpRequest = CurlHttpRequest('https://www.example.com', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  data: {
-    message: 'Hello, world!',
-  },
-});
-
-console.log(httpRequest.status()); // Output: The status code of the request
-console.log(httpRequest.responseText()); // Output: The response body as a string
-
-/****************************************************************************** */
-/* End of local dev code */
-/****************************************************************************** */
-
 // event_url is an optional extra
 const required_input = [
-  'ONE_UPTIME_URL',
-  'ONE_UPTIME_API_KEY',
-  'ONE_UPTIME_PROJECT_ID',
+  'SERVER_URL', // deployment of the code in ../server
 
   // Event severity values:
   // 0: Not classified
@@ -95,49 +20,36 @@ const required_input = [
   'event_id'
 ];
 
-// WIP development consts
-const zabbix_trigger_id = '1';
-const one_uptime_test_monitor_id = '8580c68b-a5b7-41fa-a925-46f498bc954a';
-
-// Maps Zabbix trigger id's to their associated OneUptime monitor id
-const monitor_map = {
-  zabbix_trigger_id: one_uptime_test_monitor_id,
-};
-
-var OneUptime = {
+var ZabbixOneUptimeBridgeServer = {
   validate: function (params) {
     required_input.forEach(function (key) {
+      ZabbixOneUptimeBridgeServer.params = {}
       if (key in params && params[key] != undefined && params[key] !== '') {
-        OneUptime[key] = params[key];
+        ZabbixOneUptimeBridgeServer.params[key] = params[key];
       } else {
         console.log(params);
         throw 'Missing value for key: ' + key;
       }
     });
 
-    OneUptime.event_severity = parseInt(OneUptime.event_severity);
-    OneUptime.event_is_problem = parseInt(OneUptime.event_is_problem);
-    OneUptime.event_is_update = parseInt(OneUptime.event_is_update);
+    ZabbixOneUptimeBridgeServer.raw_params = params
 
-    if (
-      typeof params.event_url === 'string' &&
-      params.event_url.trim() !== ''
-    ) {
-      OneUptime.event_url = params.event_url;
-    }
+    ZabbixOneUptimeBridgeServer.params.event_severity = parseInt(ZabbixOneUptimeBridgeServer.params.event_severity);
+    ZabbixOneUptimeBridgeServer.params.event_is_problem = parseInt(ZabbixOneUptimeBridgeServer.params.event_is_problem);
+    ZabbixOneUptimeBridgeServer.params.event_is_update = parseInt(ZabbixOneUptimeBridgeServer.params.event_is_update);
 
-    if (OneUptime.event_is_problem == 1) {
-      if (OneUptime.event_is_update == 0) {
-        OneUptime.kind = 'problem';
+    if (ZabbixOneUptimeBridgeServer.event_is_problem == 1) {
+      if (ZabbixOneUptimeBridgeServer.event_is_update == 0) {
+        ZabbixOneUptimeBridgeServer.kind = 'problem';
       } else {
-        OneUptime.kind = 'update';
+        ZabbixOneUptimeBridgeServer.kind = 'update';
       }
     } else {
-      OneUptime.kind = 'recovery';
+      ZabbixOneUptimeBridgeServer.kind = 'recovery';
     }
   },
 
-  createIncident: function (incident) {
+  forwardToServer: function () {
     const required_fields_to_create_incident = [];
     required_fields_to_create_incident.forEach(function (key) {
       if (!(key in incident)) {
@@ -145,54 +57,40 @@ var OneUptime = {
       }
     });
 
-    const path = '/api/incident';
+    const path = '/api/zabbix_webhook';
     var request = new CurlHttpRequest();
     request.AddHeader('Content-Type: application/json');
-    // https://oneuptime.com/reference/authentication
-    request.AddHeader('Authorization: Bearer ' + OneUptime.ONE_UPTIME_API_KEY);
-    request.AddHeader('ProjectID: ' + OneUptime.ONE_UPTIME_PROJECT_ID);
+    var url = ZabbixOneUptimeBridgeServer.SERVER_URL + path;
 
-    var url = OneUptime.ONE_UPTIME_URL + path;
+    console.log('[ZabbixOneUptimeBridgeServer Webhook] new request to: ' + url);
 
-    console.log('[OneUptime Webhook] new request to: ' + url);
-
-    // Note `data:` is added here
-    var blob = request.Post(url, JSON.stringify({ data: incident }));
+    var blob = request.Post(url, JSON.stringify({ data: ZabbixOneUptimeBridgeServer.raw_params }));
 
     var resp = JSON.parse(blob);
 
     if (request.Status() !== 200) {
-      if (request.Status() == 403) {
-        throw '403 creating incident';
-      }
-
       console.error(
-        '[OneUptime Webhook] Request failed, status ' +
+        '[ZabbixOneUptimeBridgeServer Webhook] Request failed, status ' +
           request.Status() +
           ': ' +
           resp.error
       );
       throw 'Request failed: ' + request.Status() + ' ' + resp.error;
     }
-    const incident_id = resp['_id'];
-    console.log('[OneUptime Webhook] created incident with id: ' + incident_id);
+    // console.log('[ZabbixOneUptimeBridgeServer Webhook] created incident with id: ' + incident_id);
   },
 
-  mapZabbixEventToIncident: function (zabbix_event) {
-    throw new Error(`mapZabbixEventToIncident not implemented.`);
-  },
 };
 
 try {
   var params = JSON.parse(value);
   console.log(params);
-  OneUptime.validate(params);
-  if (OneUptime.kind == 'problem') {
-    const incident = OneUptime.mapZabbixEventToIncident(params);
-    OneUptime.createIncident(incident);
-  }
+  ZabbixOneUptimeBridgeServer.validate(params);
+  // if (ZabbixOneUptimeBridgeServer.kind == 'problem') {
+    ZabbixOneUptimeBridgeServer.forwardToServer();
+  // }
   return 'OK';
 } catch (error) {
-  console.error('[OneUptime Webhook] Error: ' + error);
+  console.error('[ZabbixOneUptimeBridgeServer Webhook] Error: ' + error);
   throw 'Sending failed: ' + error;
 }

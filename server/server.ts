0b21e4053b4ae1d7517292451dfe3d181b15043c
Ryan Cocks <ryan@ryancocks.net>
2024-07-05 16:48:28 +0100

    app.get(`/api/v1/zabbix-alerts/:severity

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,6 @@
 #!./node_modules/.bin/ts-node
 
-// dependencies: express, typescript
-
+require('dotenv').config();
 import express from 'express';
 
 const app = express();
@@ -9,6 +8,12 @@ const port = process.env.PORT || 3168;
 
 app.use(express.json());
 
+const zabbix_severity: { [key: string]: number } = {
+  warning: 0,
+  disaster: 4,
+};
+
+// To be called by Zabbix Media Type is alert us there's been a change in monitoring status
 app.post('/api/zabbix_webhook', (req, res) => {
   // Handle the webhook data from Zabbix
   const data = req.body;
@@ -16,7 +21,25 @@ app.post('/api/zabbix_webhook', (req, res) => {
 
   // Perform any actions based on the webhook data (e.g., send notifications, store data)
 
-  res.json({ Status: "OK" })
+  res.json({ Status: 'OK' });
+});
+
+app.get(`/api/v1/zabbix-alerts/:severity`, (req, res) => {
+  const severity: number | undefined = zabbix_severity[req.params.severity];
+  if (typeof severity === 'undefined') {
+    res.status(400);
+    res.json({
+      Status: `Severity level '${severity}' not recognised, use one of ${Object.keys(
+        zabbix_severity
+      ).join(', ')}`,
+    });
+  }
+  const data = req.body;
+  console.log('Received Zabbix webhook data:', data);
+
+  // Perform any actions based on the webhook data (e.g., send notifications, store data)
+
+  res.json({ Status: 'OK' });
 });
 
 app.listen(port, () => {
